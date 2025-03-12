d66c1fae15bf95bf3c1616427748851bd51c05bf
donghwan <kdhe9797@naver.com>
2025-03-12 16:27:41 +0900

    리액트 훅

--- ch03/ch03_1/src/App.tsx
+++ ch03/ch03_1/src/App.tsx
@@ -1,13 +1,13 @@
 import ButtonTest from './pages/ButtonTest'
 import InputTest from './pages/InputTest'
-// import ModalTest from './pages/ModalTest'
+import ModalTest from './pages/ModalTest'
 
 export default function App() {
   return (
     <main>
       <ButtonTest />
       <InputTest />
-      {/* <ModalTest /> */}
+      <ModalTest />
     </main>
   )
 }
